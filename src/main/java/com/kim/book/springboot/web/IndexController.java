64f2df1cbe4a70147f7469ddf93997145e672b87
daegeon <bazzi113@naver.com>
2021-08-01 20:17:39 +0900

    세 번째 커밋

--- src/main/java/com/kim/book/springboot/web/IndexController.java
+++ src/main/java/com/kim/book/springboot/web/IndexController.java
@@ -1,4 +1,6 @@
 package com.kim.book.springboot.web;
+import com.kim.book.springboot.config.auth.dto.SessionUser;
+import com.kim.book.springboot.domain.user.User;
 import com.kim.book.springboot.service.PostsService;
 import com.kim.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,15 +9,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
         private final PostsService postsService;
+        private final HttpSession httpSession;
 
         @GetMapping("/")
         public String index(Model model){
             model.addAttribute("posts", postsService.findAllDesc());
+
+            SessionUser user = (SessionUser) httpSession.
+                                    getAttribute("user");
+
+            if(user != null){
+                model.addAttribute("userName", user.getName());
+            }
+
             return "index";
         }
 
