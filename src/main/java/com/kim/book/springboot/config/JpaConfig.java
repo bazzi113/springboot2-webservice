dbe8ea742f1f477085e8e8b5ddb1977e733c422b
daegeon <bazzi113@naver.com>
2021-10-27 23:37:22 +0900

    세 번째 커밋

---
+++ src/main/java/com/kim/book/springboot/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package com.kim.book.springboot.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
